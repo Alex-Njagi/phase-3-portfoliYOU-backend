2d4197c8a83ac772d9126b096bfd3611e7d0c2f7
Alex Njagi <1992anjagi@gmail.com>
2023-06-06 11:31:31 +0000

    CRUD for works complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,17 @@ class ApplicationController < Sinatra::Base
       name: params[:name],
       age: params[:age]
     )
-    new_artist.to_json
+    new_artist.to_json(include: :works)
   end
 
   get "/artists" do
     artists = Artist.all
-    artists.to_json
+    artists.to_json(include: :works)
   end
 
   get "/artists/:id" do
     artist = Artist.find(params[:id])
-    artist.to_json
+    artist.to_json(include: :works)
   end
 
   patch "/artists/:id" do
@@ -30,13 +30,23 @@ class ApplicationController < Sinatra::Base
       name: params[:name],
       age: params[:age]
     )
-    updated_artist.to_json
+    updated_artist.to_json(include: :works)
   end
 
   delete "/artists/:id" do
     del_artist = Artist.find(params[:id])
     del_artist.destroy
-    del_artist.to_json
+    del_artist.to_json(include: :works)
+  end
+
+  post "/works" do
+    new_work = Work.create(
+      title: params[:title],
+      work_url: params[:work_url],
+      artist_id: params[:artist_id],
+      liked: params[:liked]
+    )
+    new_work.to_json
   end
 
   get "/works" do
@@ -47,6 +57,23 @@ class ApplicationController < Sinatra::Base
   get "/works/:id" do
     work = Work.find(params[:id])
     work.to_json
+  end
+
+  patch "/works/:id" do
+    updated_work = Work.find(params[:id])
+    updated_work.update(
+      title: params[:title],
+      work_url: params[:work_url],
+      artist_id: params[:artist_id],
+      liked: params[:liked]
+    )
+    updated_work.to_json
+  end
+
+  delete "/works/:id" do
+    del_work = Work.find(params[:id])
+    del_work.destroy
+    del_work.to_json
   end
 
 end
